20de82bd28e54eaf0408841887d89c447d6a24eb
Ben Dechrai <ben@dechrai.com>
2018-08-13 15:27:27 +1000

    Added working code for end of step 2, ready for step 3

---
+++ docroots/step3/resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 contacts">
+            <div class="panel panel-default">
+                <div class="panel-heading">Contacts</div>
+                <div class="panel-body">
+                @foreach ($users as $user)
+                    <a href="">{{ $user->email }}</a>
+                @endforeach
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 messages">
+            <div class="panel panel-default">
+                <div class="panel-heading">Message</div>
+                <div class="panel-body">
+                    Select a user on the left to start chatting with them.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
